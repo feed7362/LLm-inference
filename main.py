5815995297ec485445dda3d7ccdb43f468f9e011
Kostiantyn L <feed7362@gmail.com>
2025-03-30 09:22:28 +0300

    initial model for gpu

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from llama_cpp import Llama
-# installing the package
-# llm = Llama.from_pretrained(
-#     repo_id="unsloth/DeepSeek-R1-Distill-Qwen-1.5B-GGUF",
-#     filename="DeepSeek-R1-Distill-Qwen-1.5B-Q5_K_M.gguf",
-# )   
 
+model = Llama(model_path="models/nlp/Mahou-1.5-Qwen2.5-1.5B.Q4_K_S.gguf",
+              n_gpu_layers=30,
+              n_threads=8,
+              n_batch=512,
+              use_mlock=True,
+              use_mmap=True,
+              n_ctx=4096
+              )
 
-model = Llama(model_path="models/nlp/DeepSeek-R1-Distill-Qwen-1.5B-Q5_K_M.gguf")
-response = model("Tell me a joke", max_tokens=500)
-print(response["choices"][0]["text"])
+response = model("Q: Name the number of planets in the solar system? A: ",
+                 stop=["Q:", "\n"],
+                 echo=True,
+                 max_tokens=512,
+                 temperature=0.7,
+                 top_p=0.9,
+                 top_k=35,
+                 stream=True
+                 )
+for chunk in response:
+    print(chunk["choices"][0]["text"], end="") 
